d0f99bf7993d2908994a55cb8d87df039902b1da
Nathan Van Bignoot <chocolatee.blanc_0t@icloud.com>
2023-12-05 21:48:14 +0100

    fix : syntaxe in index.html + modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalCloseBtn = document.querySelector(".close")
-const formData = document.querySelectorAll(".formData");
+//const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
